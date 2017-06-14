50830145bada654843fd15d6d8425e1fc53ca368
Rafael Vicio <rafaelvicio@icloud.com>
2017-06-13 23:20:38 -0300

    Pequenas correções na autenticação
    Falta umas coisas ainda

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,11 +1,17 @@
 var passport = require('passport');
 
-module.exports = function(app){
+module.exports = function(app) {
 
-  app.get('/auth/github', passport.authenticate('github'));
-  app.get('/auth/github/callback',
-            passport.authenticate('github', {
-                sucessRedirect: '/'
-            }));
+	app.get('/auth/github', passport.authenticate('github'));
+	app.get('/auth/github/callback',
+			passport.authenticate('github', {
+		successRedirect: '/painel/index'
+	}));
 
-};
+	app.get('/logout', function(req, res) {
+	  req.logOut(); // exposto pelo passport
+	  res.redirect('/');
+	});
+g
+
+}
