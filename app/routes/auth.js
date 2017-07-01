b22e588c423caae3480b4e067f996a4501e01fc9
Rafael Vicio <rafaelvicio@icloud.com>
2017-07-01 01:11:41 -0300

    Corrigindo autenticação

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,10 +2,15 @@ var passport = require('passport');
 
 module.exports = function(app){
 
-  app.get('/auth/github', passport.authenticate('github'));
-  app.get('/auth/github/callback',
-            passport.authenticate('github', {
-                sucessRedirect: '/'
-            }));
+  app.get('/auth/github', 
+        passport.authenticate('github'));
+    app.get('/auth/github/callback',
+        passport.authenticate('github', {
+        failureRedirect: '/'}),
+        function (req, res) {
+            
+        //sucesso na autenticação
+        res.redirect('/#!/painel/index');
+    });
 
 };
