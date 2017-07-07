37d864f2da7b478196a8fbc27cafeb2396266d65
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-07-07 16:57:12 -0300

    Alterando config mongodb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')('localhost/esportteam');
+require('./config/database')('localhost/esc');
 //require('./config/database')('mongodb://esportsteam:esportsteam@ds123752.mlab.com:23752/esports-team');
 
 http.createServer(app).listen(3000, function(){
