39851e2471e057b1b8caaa959df21a90266a8af5
Jean Carlo Nascimento <jnascimento@gmail.com>
2017-07-07 16:58:52 -0300

    Arrumei soh uma para vc ver

--- app/api/usuario.js
+++ app/api/usuario.js
@@ -1,76 +1,77 @@
-var mongoose = require('mongoose');
-var api = {};
+const mongoose = require('mongoose')
+const model = mongoose.model('Usuario')
 
-var model = mongoose.model('Usuario');
+  // arrumei soh esse
+const adiciona = (req, res) => 
+  model.create(req.body)
+        .then( ( usuario ) => res.json( usuario ) )
+        .catch( ( error ) => {
+          console.log( error )
+          res.status( 500 ).json( error )
+        } )
 
-api.adiciona = function(req, res) {
-
-  model
-    .create(req.body)
-      .then(function(){
-        res.json(usuario);
-
-      }, function(error) {
-        console.log(error);
-        res.status(500).json(error);
-      });
-
-};
-
-api.atualiza = function(req, res) {
+const atualiza = function(req, res) {
 
   model
     .findByIdAndUpdate(req.params.id, req.body)
       .then(function(usuario) {
-        res.json(usuario);
+        res.json(usuario)
 
       }, function(error) {
-        console.log(error);
-        res.status(500).json(error);
-      });
+        console.log(error)
+        res.status(500).json(error)
+      })
 
-};
+}
 
-api.lista = function(req, res) {
+const lista = function(req, res) {
 
   model
     .find({})
     .then(function(usuarios) {
-      res.json(usuarios);
+      res.json(usuarios)
 
     }, function(error) {
-      console.log(error);
-      res.status(500).json(error);
-    });
+      console.log(error)
+      res.status(500).json(error)
+    })
 
-};
+}
 
-api.buscaPorId = function(req, res) {
+const buscaPorId = function(req, res) {
 
   model
     .findById(req.params.id)
       .then(function(usuario){
-        if(!usuario) throw Error('usuario não encontrada');
-        res.json(usuario);
+        if(!usuario) throw Error('usuario não encontrada')
+        res.json(usuario)
 
       }, function(error){
-        console.log(error);
-        res.status(500).json(error);
-      });
+        console.log(error)
+        res.status(500).json(error)
+      })
 
-};
+}
 
-api.removePorId = function(req, res) {
+const removePorId = function(req, res) {
 
   model
     .remove({_id: req.params.id})
       .then(function(){
-        res.sendStatus(204);
+        res.sendStatus(204)
       }, function(error) {
-        console.log(error);
-        res.status(500).json(error);
-      });
+        console.log(error)
+        res.status(500).json(error)
+      })
 
-};
+}
 
-module.exports = api;
+const api = {
+  adiciona,
+  atualiza,
+  lista,
+  buscaPorId,
+  removePorId
+}
+
+module.exports = api
