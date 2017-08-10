e1ad1473bd7fc794e2a94e79d7fbb79b761f7613
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-08-10 13:37:05 -0300

    Alterando formulario

--- Front/src/app/login-usuario/login-usuario.component.ts
+++ Front/src/app/login-usuario/login-usuario.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsuarioComponent } from '../usuario/usuario.component';
+import { UsuarioService } from '../usuario/usuario.service.component';
+
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-login-usuario',
   templateUrl: './login-usuario.component.html',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginUsuarioComponent implements OnInit {
 
-  constructor() { }
+  usuario: UsuarioComponent = new UsuarioComponent();
+
+  constructor( private service: UsuarioService, private router: ActivatedRoute) { 
+    this.service = service;
+    this.router = router;
+  }
 
   ngOnInit() {
+  }
+
+  logar(){
+
+      this.service.logar(this.usuario)
+        .then( res => {
+          console.log('Logando...')
+        })
+        .catch(erro => console.log(erro))
+        
   }
 
 }
