cd5c45d7748e814be12a954d8f7a55b441564fb6
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-08-25 14:58:10 -0300

    Verificando se ususario esta logado

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from './usuario/usuario.service.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(public usuario: UsuarioService) {}
+
 }
